4e8bb7e948d63d1579c21cfa30fc2ac63c569022
Miguel A. Gonzales <antoniogonzales2010@gmail.com>
2018-06-01 14:59:35 -0500

    Initial commit

---
+++ app/src/main/java/pe/blogspot/tknof/mhtski_pc/MainActivity.java
@@ -0,0 +1,21 @@
+package pe.blogspot.tknof.mhtski_pc;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
